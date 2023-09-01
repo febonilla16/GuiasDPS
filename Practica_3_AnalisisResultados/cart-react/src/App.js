e5618562112a65bdf6900d128020269215e3e5b0
febonilla16 <fernandobonilla875@gmail.com>
2023-08-31 21:57:45 -0600

    Guias Practicas DPS

---
+++ Practica_3_AnalisisResultados/cart-react/src/App.js
@@ -0,0 +1,40 @@
+
+import { useState } from 'react'; 
+import { Headers } from './components/Headers'; 
+import  Modals  from './components/Modals';
+
+
+
+
+function App() { 
+
+
+  const [allProducts, setAllProducts] = useState([]); 
+  const [total, setTotal] = useState(0); 
+  const [countProducts, setCountProducts] = useState(0);
+
+
+  return ( 
+    <> 
+    <Headers 
+      allProducts={allProducts} 
+      setAllProducts={setAllProducts} 
+      total={total} setTotal={setTotal} 
+      countProducts={countProducts} 
+      setCountProducts={setCountProducts} />  
+    <Modals
+    allProducts={allProducts} 
+    setAllProducts={setAllProducts}
+    total={total} 
+    setTotal={setTotal} 
+    countProducts={countProducts}
+    setCountProducts={setCountProducts}
+    />
+    
+    </> ); 
+    } 
+
+
+
+
+  export default App;
